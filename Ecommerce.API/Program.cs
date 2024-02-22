0afbce78de9fe8b589fdd41d098286c29b163a5f
Sergio De Luca <serg.r.deluca@gmail.com>
2024-02-21 23:18:01 -0300

    controladores e instalacion de swagger

--- Ecommerce.API/Program.cs
+++ Ecommerce.API/Program.cs
@@ -6,6 +6,7 @@ using Ecommerce.Repositorio.Implementacion;
 using Ecommerce.Servicio.Contrato;
 using Ecommerce.Servicio.Implementacion;
 using Ecommerce.Utilidades;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -36,12 +37,37 @@ builder.Services.AddScoped<IVentaServicio, VentaServicio>();
 
 builder.Services.AddScoped<IDashboardServicio, DashboardServicio>();
 
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("nueva", app =>
+    {
+        app.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+
+app.UseSwagger();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
 app.UseHttpsRedirection();
+
+
+
+app.UseCors("nueva");
 
 app.UseAuthorization();
 
